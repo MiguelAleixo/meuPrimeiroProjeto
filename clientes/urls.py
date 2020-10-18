6f7b7c8d02ea03eea11b65f4eaef34b623dac08b
Miguel Aleixo <miguel.lima@luizalabs.com>
2020-10-18 20:24:08 -0300

    finish crud

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import persons_list
+from .views import persons_list, persons_new, persons_update, persons_delete
 
 urlpatterns = [
-    path('list/', persons_list),
+    path('list/', persons_list, name='person_list'),
+    path('new/', persons_new, name='person_new'),
+    path('update/<int:id>', persons_update, name='person_update'),
+    path('delete/<int:id>', persons_delete, name='person_delete'),
 ]
--- meuPrimeiroProjeto/urls.py
+++ meuPrimeiroProjeto/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 from clientes import urls as clients_urls
 
 urlpatterns = [
+                  path('admin/', admin.site.urls),
                   path('person/', include(clients_urls)),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
